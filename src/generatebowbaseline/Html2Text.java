8f9f02b5afd57c3835d3d4827b844e3ee666aaca
luis <Luis Miguel>
2015-06-14 22:34:24 +0200

    Implementado método que maneja los tags HTML individuales.

--- src/generatebowbaseline/Html2Text.java
+++ src/generatebowbaseline/Html2Text.java
@@ -1,6 +1,7 @@
 package generatebowbaseline;
 
 import java.io.*;
+import javax.swing.text.MutableAttributeSet;
 import javax.swing.text.html.*;
 import javax.swing.text.html.parser.*;
 
@@ -11,8 +12,11 @@ import javax.swing.text.html.parser.*;
 public class Html2Text extends HTMLEditorKit.ParserCallback
 {
    StringBuffer s;
+   int hasEmoticons;
 
-     public Html2Text() {}
+     public Html2Text() {
+         hasEmoticons = 0;
+     }
 
      public void parse(Reader in) throws IOException {
        s = new StringBuffer();
@@ -25,8 +29,37 @@ public class Html2Text extends HTMLEditorKit.ParserCallback
      public void handleText(char[] text, int pos) {
        s.append(text);
      }
+    
+     /* No de momento
+    @Override
+    public void handleStartTag(HTML.Tag t, MutableAttributeSet a, int pos) {
+        System.out.println(t.toString());
+    }
+
+    @Override
+    public void handleEndTag(HTML.Tag t, int pos) {
+        System.out.println(t.toString());
+    }
+     */
+
+    @Override
+    public void handleSimpleTag(HTML.Tag t, MutableAttributeSet a, int pos) {
+        // Si la etiqueta rompe el flujo de texto como br anyadimos espacio
+        // para evitar juntar dos palabras diferentes
+        if(t.breaksFlow())
+            s.append(' ');
+        
+        // Si es una imagen nos quedamos el atributo alt
+        if(t == HTML.Tag.IMG) {
+            hasEmoticons = 1;
+            //System.out.println("Imagen "  + a.getAttribute(HTML.Attribute.CLASS) + " " + a.getAttribute(HTML.Attribute.SRC));
+            //System.out.println(s.toString());
+        }
+    }
 
      public String getText() {
        return s.toString();
      }
+     
+    
 }
